26c117f8ebf861cc0cade8d02fd51389a83769af
ElenaBaninemeh <elena.baninemeh@yahoo.com>
2020-08-04 03:25:44 +0430

    Updates

---
+++ precache-manifest.687f4e1eaabba4ef0278ed7418bcf7a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b613d1bd71be7751e078b51940dc7dae",
+    "url": "/MydApp/index.html"
+  },
+  {
+    "revision": "a6b2d2ad9fd5cf54b95f",
+    "url": "/MydApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d942e1a81f7d82ca506",
+    "url": "/MydApp/static/js/2.bb516fac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MydApp/static/js/2.bb516fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b2d2ad9fd5cf54b95f",
+    "url": "/MydApp/static/js/main.50efee6a.chunk.js"
+  },
+  {
+    "revision": "71e5fa8a9ff5898a6e27",
+    "url": "/MydApp/static/js/runtime-main.cb3f4d15.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MydApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
