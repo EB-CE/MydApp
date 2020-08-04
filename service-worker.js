dd32716c0d31b2b13d9af30503d8d631bbe0df72
ElenaBaninemeh <elena.baninemeh@yahoo.com>
2020-08-04 04:33:14 +0430

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MydApp/precache-manifest.22ed56631cb8e478b93dd6c0a94f6a71.js"
+  "/MydApp/precache-manifest.4624e7af24599862d323a7a8abfcdf76.js"
 );
 
 self.addEventListener('message', (event) => {
